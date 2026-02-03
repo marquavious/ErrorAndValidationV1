0ab4dde83a8c6df6ba11de6d8cf017b2acccd99f
marquavious <marquavious@users.noreply.github.com>
2026-02-02 19:07:38 -0500

    Initial Commit

---
+++ ErrorAndValidationV1/ErrorAndValidationV1App.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorAndValidationV1App.swift
+//  ErrorAndValidationV1
+//
+//  Created by Marquavious Draggon on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ErrorAndValidationV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
